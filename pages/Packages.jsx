ac62a6d8918818fe21f528910904354341deabc8
AliNayab Nathani <alinayab@inspedium.com>
2023-09-14 10:09:48 +0500

    signin

--- pages/Packages.jsx
+++ pages/Packages.jsx
@@ -1,39 +1,55 @@
-import { Button, Flex, FormLabel, HStack, Input } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormLabel,
+  HStack,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
 import { PageHeading } from "../components/Admin/SmallReusableComponents/Heading";
 import PackageTable from "../components/Admin/Packages/PackageTable";
 import { useSearchContext } from "../components/Admin/Context api/Context";
 
 const ColumnSearch = () => {
-    const { searchQuery, updateSearchQuery, updateFilter } = useSearchContext();
+  const { searchQuery, updateSearchQuery, updateFilter } = useSearchContext();
 
-    const handleClear = () => {
-        // setStartDate(new Date());
-        updateSearchQuery('');
-    };
-    return (
-        <HStack width={'100%'} justifyContent={'flex-end'} >
-            <Flex >
-                <FormLabel>
-                    Search All Columns
-                </FormLabel>
-                <Input value={searchQuery} onChange={(e) => updateSearchQuery(e.target.value)} />
-                <Button ml={3} variant={'outline'}
-                    onClick={() => updateFilter()}>
-                    Apply
-                </Button>
-                <Button ml={3} variant={'solid'}
-                    onClick={handleClear}
-                >Clear</Button>
-            </Flex>
-        </HStack>
-    );
-}
+  const handleClear = () => {
+    // setStartDate(new Date());
+    updateSearchQuery("");
+  };
+  return (
+    <Stack
+      justifyContent={["center", "flex-end"]}
+      direction={["column", "row"]}
+      width={["full", "full"]}
+      alignItems={"center"}
+      my={[0, 12]}
+    >
+      <Input
+        value={searchQuery}
+        onChange={(e) => updateSearchQuery(e.target.value)}
+        placeholder="Search All Column"
+        bg={"#232323"}
+        focusBorderColor="#232323"
+        w={["full", "30%"]}
+      />
+      <HStack my={[4, 0]}>
+        <Button ml={3} variant={"outline"} onClick={() => updateFilter()}>
+          Apply
+        </Button>
+        <Button ml={3} variant={"solid"} onClick={handleClear}>
+          Clear
+        </Button>
+      </HStack>
+    </Stack>
+  );
+};
 export default function Packages() {
-    return (
-        <>
-            <PageHeading text={'Packages'} />
-            <ColumnSearch />
-            <PackageTable />
-        </>
-    );
+  return (
+    <>
+      <PageHeading text={"Packages"} />
+      <ColumnSearch />
+      <PackageTable />
+    </>
+  );
 }
