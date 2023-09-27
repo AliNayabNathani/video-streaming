3c1d3998b299c2e07dda893cbd9b28e12f80a72b
AliNayab Nathani <alinayab@inspedium.com>
2023-09-27 10:22:28 +0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,14 @@ import AdminLayout from "./Admin/AdminLayout";
 import ClientLayout from "./Client/ClientLayout";
 
 function MyApp({ Component, pageProps }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    // Determine which layout to use based on the route
-    if (router.pathname.startsWith("/Admin")) {
-        return <AdminLayout Component={Component} pageProps={pageProps} />;
-    } else {
-        return <ClientLayout Component={Component} pageProps={pageProps} />;
-    }
+  // Determine which layout to use based on the route
+  if (router.pathname.startsWith("/Admin")) {
+    return <AdminLayout Component={Component} pageProps={pageProps} />;
+  } else {
+    return <ClientLayout Component={Component} pageProps={pageProps} />;
+  }
 }
 
 export default MyApp;
